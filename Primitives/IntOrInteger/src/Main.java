d558b3688344a0594dc4b24794cdb05ebc703bef
tamila_popova <popova.t.t@yandex.ru>
2024-01-29 21:40:26 +0300

    alphabet

--- Primitives/IntOrInteger/src/Main.java
+++ Primitives/IntOrInteger/src/Main.java
@@ -2,11 +2,28 @@ public class Main {
     public static void main(String[] args) {
         Container container = new Container();
         container.addCount(5672);
-        System.out.println(container.getCount());
+        System.out.println(container.getCount() + "\n");
 
         // TODO: ниже напишите код для выполнения задания:
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
 
+
+        char a = 'Ё';
+        int b = a;
+        char c = 'ё';
+        int d = c;
+
+        for (int i = 1040; i <= 1103; i++) {
+            char A = (char) i;
+            if (i == 1046) {
+                System.out.println(b + " - " + a);
+            } else if (i == 1078) {
+                System.out.println(d + " - " + c);
+            }
+            System.out.println(i + " - " + A);
+        }
     }
+
+
 }
