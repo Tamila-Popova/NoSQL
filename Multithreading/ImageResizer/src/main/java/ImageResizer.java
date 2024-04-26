70d196b068ff5be94388e518a01e917f87d07fba
tamila_popova <popova.t.t@yandex.ru>
2024-04-26 19:34:17 +0300

    Task ImageResizer

--- Multithreading/ImageResizer/src/main/java/ImageResizer.java
+++ Multithreading/ImageResizer/src/main/java/ImageResizer.java
@@ -8,6 +8,10 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.awt.image.BufferedImageOp;
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Getter
 @Setter
@@ -20,12 +24,24 @@ public class ImageResizer extends Thread {
     public void run() {
         try {
             for (File file : files) {
+
                 BufferedImage image = ImageIO.read(file);
+                Pattern p1 = Pattern.compile("\\w+\\.[png]");
+                Matcher m1 = p1.matcher(file.getName());
+                Pattern p2 = Pattern.compile("\\w+\\.[jpg]");
+                Matcher m2 = p2.matcher(file.getName());
                 int newWidth = 300;
                 int newHeight = (int) Math.round(image.getHeight() / (image.getWidth() / (double) newWidth));
-                BufferedImage imageSc = Scalr.resize(image, Scalr.Method.BALANCED, newWidth, newHeight);
-                File newFile = new File(dstFolder + "/" + file.getName());
-                ImageIO.write(imageSc, "jpg", newFile);
+                if (m1.matches() == false) {
+                    BufferedImage imageSc = Scalr.resize(image, Scalr.Method.BALANCED, newWidth, newHeight);
+                    File newFile = new File(dstFolder + "/" + file.getName());
+                    ImageIO.write(imageSc, "png", newFile);
+                }
+                if (m2.matches() == false) {
+                    BufferedImage imageSc = Scalr.resize(image, Scalr.Method.BALANCED, newWidth, newHeight);
+                    File newFile = new File(dstFolder + "/" + file.getName());
+                    ImageIO.write(imageSc, "jpg", newFile);
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
