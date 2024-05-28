1769a2358a09ebf895b5fcc1d9b63428a6f66f61
tamila_popova <popova.t.t@yandex.ru>
2024-05-28 15:04:36 +0300

    Task Site

--- Multithreading/Site/src/main/java/Main.java
+++ Multithreading/Site/src/main/java/Main.java
@@ -2,20 +2,19 @@ import java.util.List;
 import java.util.concurrent.ForkJoinPool;
 
 public class Main {
-
-
     public static void main(String[] args) {
-        String url = "https://www.bio-faq.ru/";
-       Links pageNode = new Links(url);
-       new ForkJoinPool().invoke(new Parser(pageNode));
-       List<Links> parserList = pageNode.getChildLinks();
-       printLinks(pageNode, 0);
+        String url = "https://www.bio-faq.ru";
+        Links pageNode = new Links(url);
+        new ForkJoinPool().invoke(new Parser(pageNode));
+        printLinks(pageNode, 0);
     }
-    private static void printLinks(Links root, int depth){
-        for (int i=0; i<depth; i++){
-            System.out.println("\t");
+
+    private static void printLinks(Links root, int depth) {
+        for (int i = 0; i < depth; i++) {
+           System.out.println(root.getUrl());
         }
-        System.out.println(root.getUrl());
-        root.getChildLinks().forEach(link -> {printLinks(link, depth+1);});
+        root.getChildLinks().forEach(link -> {
+            printLinks(link, depth+1);
+        });
     }
 }
--- Multithreading/Site/src/main/java/Parser.java
+++ Multithreading/Site/src/main/java/Parser.java
@@ -1,12 +1,9 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import java.util.concurrent.RecursiveTask;
 
 public class Parser extends RecursiveTask<Links> {
@@ -16,35 +13,17 @@ public class Parser extends RecursiveTask<Links> {
         this.links = links;
     }
 
-    Set<String> uniqueURLs;
-
     @Override
     protected Links compute() {
-        String domain = "https://[^,//s]+";
         try {
-            uniqueURLs = new HashSet<>();
-            Thread.sleep(200);
+            Thread.sleep(300);
             Document doc = Jsoup.connect(links.getUrl()).get();
-            Elements elements = doc.select("a");
-            elements.stream()
-                    .map((l) ->
-                            l.attr("abs:href"))
-                    .forEachOrdered((subUrl) -> {
-                        if (subUrl.contains(domain) && subUrl.endsWith("/")) {
-                            boolean added = uniqueURLs.add(subUrl);
-                            if (added) {
-                                links.addChildURL(subUrl);
-                            }
-                      }
-                    });
-            List<Parser> taskList = new ArrayList<>();
-            for(Links child : links.getChildLinks()){
-                Parser task = new Parser(child);
-                task.fork();
-                taskList.add(task);
-            }
-            for(Parser task : taskList){
-                task.join();
+            Elements elements = doc.select("a[href]");
+            for (Element element : elements) {
+                String attrUrl = element.absUrl("href");
+                if (!attrUrl.isEmpty() && !attrUrl.contains(links.getUrl())) {
+                    links.addChildURL(attrUrl);
+                }
             }
         } catch (InterruptedException | IOException e) {
             throw new RuntimeException(e);
@@ -52,3 +31,4 @@ public class Parser extends RecursiveTask<Links> {
         return links;
     }
 }
+
