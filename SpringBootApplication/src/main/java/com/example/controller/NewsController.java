6b99f54540be63ce7f0b0f723bcf3a395ee17fcb
Tamila Popova <popova.t.t@yandex.ru>
2024-06-18 20:07:56 +0300

    News

--- SpringBootApplication/src/main/java/com/example/controller/NewsController.java
+++ SpringBootApplication/src/main/java/com/example/controller/NewsController.java
@@ -1,14 +1,14 @@
 package com.example.controller;
 
+import com.example.dto.InfoDto;
 import com.example.dto.NewsDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.example.services.NewsCRUDService;
 
-import javax.swing.event.HyperlinkEvent;
-import java.awt.desktop.SystemEventListener;
 import java.util.Collection;
+
 
 @RestController
 @RequestMapping("/api/news")
@@ -20,12 +20,14 @@ public class NewsController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity getById(@PathVariable Long id) {
+    public ResponseEntity<InfoDto> getById(@PathVariable Long id) {
+        String message = "Новость с ID " + id + " не найдена";
+        InfoDto infoDto = new InfoDto(message);
         if(newsCRUDService.getById(id) == null){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Новость с ID " + id + " не найдена");
+            return new ResponseEntity(infoDto, HttpStatus.NOT_FOUND);
         }
         newsCRUDService.getById(id);
-        return ResponseEntity.status(HttpStatus.OK).body(newsCRUDService.getById(id));
+        return new ResponseEntity(newsCRUDService.getById(id), HttpStatus.OK);
     }
 
     @GetMapping
@@ -46,12 +48,14 @@ public class NewsController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity deleteById(@PathVariable Long id) {
+    public ResponseEntity<InfoDto> deleteById(@PathVariable Long id) {
+        String message = "Новость с ID " + id + " не найдена";
+        InfoDto infoDto = new InfoDto(message);
         if(newsCRUDService.getById(id) == null){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Новость с ID " + id + " не найдена");
+            return new ResponseEntity(infoDto, HttpStatus.NOT_FOUND);
         }
         newsCRUDService.deleteById(id);
-        return new ResponseEntity(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
 
---
+++ SpringBootApplication/src/main/java/com/example/dto/InfoDto.java
@@ -0,0 +1,14 @@
+package com.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class InfoDto {
+    private String message;
+}
